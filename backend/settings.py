c046d3f90018ea2b22c3afc733ce677f35e1c2a4
jenifferj6748 <jenifferj6748@example.com>
2025-10-25 12:54:01 +0530

    Updated CORS origin to match frontend URL

--- backend/settings.py
+++ backend/settings.py
@@ -134,5 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS_ORIGIN_ALLOW_ALL = True 
 # CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
 CORS_ALLOWED_ORIGINS = [
-    "https://todo-frontend.vercel.app",  # your frontend URL
+   "https://todo-frontend.vercel.app",
+    "https://todo-frontend-xi-one.vercel.app",
 ]
